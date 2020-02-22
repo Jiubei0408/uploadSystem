3b61007e10778e466d5f1324a639c4dcd5f37873
Jiubei0408 <757795210@qq.com>
2020-02-22 09:51:37 +0800

    导入notification蓝图

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,6 +1,7 @@
-from app.routers import (main, session)
+from app.routers import (main, session, notifications)
 
 
 def get_blueprints():
     return [(main.bp, '/'),
-            (session.bp, '/session')]
+            (session.bp, '/session'),
+            (notifications.bp, '/notifications')]
