e32a88bb2883546e2a5f8c93fca69451b9838213
Jiubei0408 <757795210@qq.com>
2020-02-22 08:20:01 +0800

    列表界面添加管理员显示

--- app/routers/main.py
+++ app/routers/main.py
@@ -1,8 +1,10 @@
 from flask import Blueprint
-from flask_login import current_user
+from flask_login import current_user, login_required
+
 from app.forms.upload import UploadForm
 from app.forms.login import LoginForm
 from app.models.user import User
+from app.libs.auth import admin_only
 
 from flask import render_template
 
@@ -24,11 +26,12 @@ def lists():
     for i in data['data']:
         content.append([i.studentId,
                         i.nickname,
+                        ['普通用户', '管理员'][i.permission],
                         i.update_time])
 
     return render_template('list.html',
                            count=data['count'],
-                           labels=['学号', '姓名', '时间'],
+                           labels=['学号', '姓名', '权限', '时间'],
                            content=content,
                            user=current_user)
 
@@ -38,4 +41,12 @@ def login():
     form = LoginForm()
     return render_template('login.html',
                            form=form,
+                           user=current_user)
+
+
+@bp.route('/admin')
+@login_required
+@admin_only
+def admin():
+    return render_template('admin.html',
                            user=current_user)
