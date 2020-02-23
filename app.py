eb505f4cacaa300042eb6fc4c272150c7a456c46
Jiubei0408 <757795210@qq.com>
2020-02-23 10:29:52 +0800

    接受所有ip的请求

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def framework_error(e):
 
 if __name__ == '__main__':
     try:
-        app.run(port=80)
+        app.run(host='0.0.0.0', port=80)
     except Exception as e:
         print(e)
         raise e
